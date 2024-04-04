6d38cc91ef761e19968d0986e78db2fc84032ae3
balusena <balusena12@gmail.com>
2024-04-04 16:17:15 +0100

    roboshop kubernetes files

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 source /data/params
-java -XX:MaxRAMPercentage=95.0 -XX:InitialRAMPercentage=70 -jar /app/shipping.jar
+java -javaagent:/app/newrelic/newrelic.jar -XX:MaxRAMPercentage=95.0 -XX:InitialRAMPercentage=70 -jar /app/shipping.jar
